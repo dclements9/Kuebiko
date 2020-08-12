26e45c54cb1299abab0c61fcb4be43597c424cb7
Dylan Clements <dclements@protonmail.com>
2020-08-12 07:13:17 -0400

    Welcome Component

---
+++ src/Components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => (
+    <div>
+        <h1> Kuebiko </h1>
+        <h2 className='title'>Welcome to Kuebiko</h2>
+        <h3 className='title_sub'>Your Home of Japanese Myths.</h3>
+    </div>
+)
+
+export default Welcome
