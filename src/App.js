fb4bf90496e19eaa159cc698e0e8d7d740533a66
Dylan Clements <dclements@protonmail.com>
2020-08-13 16:21:09 -0400

    MythComponent on App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 // import logo from './logo.svg';
 import Welcome from './Components/Welcome';
+import MythComponent from './Components/MythComponent';
 import './App.css';
 
 function App() {
   return (
+    <div>
     <Welcome />
+    <MythComponent />
+    </div>
   );
 }
 
